7d140a63173b0631ec6d6f9bc7658c629ef45397
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2024-07-07 18:00:37 -0300

    sonar

--- src/GestaoPedidos.Application/DTOs/Pedido/CadastroPedidoDto.cs
+++ src/GestaoPedidos.Application/DTOs/Pedido/CadastroPedidoDto.cs
@@ -7,5 +7,5 @@ namespace GestaoPedidos.Application.DTOs.Pedido;
 public class CadastroPedidoDto
 {
     public int IdCliente { get; set; }
-    public IEnumerable<int> Produtos { get; set; }
+    public IEnumerable<int> Produtos { get; set; } = Enumerable.Empty<int>();
 }
